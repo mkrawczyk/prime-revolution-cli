2a8bf7684c591118c80e32f94ca51cfbf395c076
Matt Krawczyk <matthew.krawczyk@gmail.com>
2025-02-21 16:26:12 -0500

    Removed unused include.

--- src/Services/ContentService.php
+++ src/Services/ContentService.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Mkrawczyk\PrimeRevolutionCli\Services;
 
-use DOMNode;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\ServerException;
